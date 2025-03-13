2d341c431c10afa543cb16b60cc0e35065136251
Damien353 <10034@holbertonstudents.com>
2025-03-13 14:01:22 +0100

    modif return du get user by email

--- part3/app/services/facade.py
+++ part3/app/services/facade.py
@@ -25,7 +25,7 @@ class HBnBFacade:
 
     def get_user_by_email(self, email):
         """Récupère un utilisateur par son email."""
-        return self.user_repo.get_by_attribute('email', email)
+        return self.user_repo.get_user_by_email(email)
 
     def get_all_users(self):
         """Récupère tous les utilisateurs."""
