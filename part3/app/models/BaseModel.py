c4f8ec07fbe87076f174b7a053a0ca51e38c6aab
Damien353 <10034@holbertonstudents.com>
2025-03-13 10:30:20 +0100

    Modification BaseModel pour être compatible SQLAlchemy

--- part3/app/models/BaseModel.py
+++ part3/app/models/BaseModel.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
+from app import db
 import uuid
 from datetime import datetime
 
 
-class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+class BaseModel(db.Model):
+    __abstract__ = True  # Empêche SQLAlchemy de créer une table pour cette classe
 
+    # Définition des attributs comme des colonnes SQLAlchemy
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    # Méthode pour mettre à jour l'attribut updated_at lors de chaque modification
     def save(self):
-        """Update the updated_at timestamp whenever the object is modified"""
-        self.updated_at = datetime.now()
+        """Met à jour l'attribut updated_at chaque fois que l'objet est modifié."""
+        self.updated_at = datetime.utcnow()
+        db.session.commit()  # Assurer que les modifications sont persistées
 
     def update(self, data):
-        """Update the attributes of the object based on the provided dictionary"""
+        """Met à jour les attributs de l'objet à partir d'un dictionnaire de données"""
         for key, value in data.items():
             if hasattr(self, key):
                 setattr(self, key, value)
-        self.save()  # Update the updated_at timestamp
+        self.save()  # Sauvegarder l'objet et mettre à jour la date
